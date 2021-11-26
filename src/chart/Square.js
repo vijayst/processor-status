6361c8a662e450a560f8921e98374c3b04a52cd2
Vijay T <vijayst@gmail.com>
2021-11-26 14:02:13 +0530

    display square with data

--- src/chart/Square.js
+++ src/chart/Square.js
@@ -1,5 +1,41 @@
 import React from 'react';
 
-export default function Square() {
-  return <div></div>;
+function getColor(event) {
+  if (event.errorCount > 0) {
+    return 'red';
+  } else if (event.warningCount > 0) {
+    return 'yellow';
+  } else if (event.successCount > 0) {
+    return 'green';
+  } else if (event.infoCount > 0) {
+    return 'blue';
+  } else {
+    return 'transparent';
+  }
+}
+
+function getOpacity(event) {
+  if (event.errorCount > 0) {
+    return event.errorCount / 5;
+  } else if (event.warningCount > 0) {
+    return event.warningCount / 20;
+  } else if (event.successCount > 0) {
+    return event.successCount / 5;
+  } else if (event.infoCount > 0) {
+    return event.infoCount / 100;
+  }
+  return 1;
+}
+
+export default function Square({ event, size, rowIndex, timelineHeight }) {
+  return (
+    <rect
+      x={(event.tick - 1) * size}
+      y={timelineHeight + rowIndex * size}
+      width={size}
+      height={size}
+      fill={getColor(event)}
+      opacity={getOpacity(event)}
+    />
+  );
 }
--- src/chart/SquareArea.js
+++ src/chart/SquareArea.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Timeline from './Timeline';
+import Square from './Square';
 
 const MAX_SQUARES = 40;
 const WIDTH = 24;
@@ -11,6 +12,17 @@ export default function SquareArea({ data }) {
   return (
     <svg width={width} height={height}>
       <Timeline width={width} squareCount={MAX_SQUARES} squareWidth={WIDTH} />
+      {data.map((processor, rowIndex) => {
+        return processor.events.map((event) => (
+          <Square
+            key={`${processor.name}_${event.tick}`}
+            rowIndex={rowIndex}
+            event={event}
+            timelineHeight={TIMELINE_HEIGHT}
+            size={WIDTH}
+          />
+        ));
+      })}
     </svg>
   );
 }
